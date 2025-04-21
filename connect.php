6fe13f7e14582a72c08532c52b729cec56644e9c
Tasneeem <tasneemritee008@gmail.com>
2025-04-21 20:07:49 +0600

    wedding ui

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = pg_connect("host=localhost dbname=mydb user=myuser password=mypass");
-
-if (!$conn) {
-    die("Connection failed: " . pg_last_error());
-} else {
-    echo "Connected successfully!";
-}
-?>
